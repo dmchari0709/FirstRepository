91011946e2ac0a5b9283c4f617d53f5fd1e2340a
dmchari0709 <dmchari0709@gmail.com>
2019-03-16 23:44:32 +0530

    second commit

--- PracticeProject/src/main/java/madhav/com/Test.java
+++ PracticeProject/src/main/java/madhav/com/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("This files is changed in Git");
+        System.out.printlnn("This files is changed in Git");
         //adding comments
 	}
 
